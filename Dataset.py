35c1cb5dbd4b41f402fdf664a05aebfc16acd6ac
alonkaya <alon.kaya.1@gmail.com>
2024-01-05 18:35:02 +0200

    .

--- Dataset.py
+++ Dataset.py
@@ -40,10 +40,10 @@ class CustomDataset(torch.utils.data.Dataset):
         F = compute_fundamental(E, self.k, self.k)
 
         # Convert to tensor and rescale [0,255] -> [0,1]
-        first_image, second_image, F  = T.to_tensor(first_image), T.to_tensor(second_image), normalize_L2(normalize_L1(torch.tensor(F, dtype=torch.float32))), torch.tensor(F, dtype=torch.float32)
+        first_image, second_image, F, unnormalized_F  = T.to_tensor(first_image), T.to_tensor(second_image), normalize_L2(normalize_L1(torch.tensor(F, dtype=torch.float32))), torch.tensor(F, dtype=torch.float32)
         
         # TODO: Normalize images?
-        return first_image, second_image, F
+        return first_image, second_image, F, unnormalized_F
 
 transform = transforms.Compose([
     transforms.Resize(256),
