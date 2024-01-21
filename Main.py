3f97cbee64120b555e5dff8a641b2a4fd189d8e2
alonkaya <alon.kaya.1@gmail.com>
2024-01-21 13:16:17 +0200

    .

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ from FMatrixRegressor import FMatrixRegressor
 from params import *
 from Dataset import train_loader, val_loader
 import torch
-from FunMatrix import enforce_fundamental_constraint
 import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -11,7 +10,7 @@ print(device)
 model = FMatrixRegressor(mlp_hidden_sizes, num_output, pretrained_model_name=clip_model_name, lr=learning_rate, device=device, freeze_pretrained_model=False)
 model = model.to(device)
 
-print(f'learning_rate: {learning_rate}, mlp_hidden_sizes: {mlp_hidden_sizes}, jump_frames: {jump_frames},  add_penalty_loss: {add_penalty_loss}, enforce_fundamental_constraint: {enforce_fundamental_constraint}')
+print(f'learning_rate: {learning_rate}, mlp_hidden_sizes: {mlp_hidden_sizes}, jump_frames: {jump_frames},  add_penalty_loss: {add_penalty_loss}')
 model.train_model(train_loader, val_loader, num_epochs=num_epochs)
 
 
