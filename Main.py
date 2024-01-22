4c62945d5702bc22aa95d9d994803952c2e50780
alonkaya <alon.kaya.1@gmail.com>
2024-01-22 20:22:10 +0200

    .

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ print(device)
 model = FMatrixRegressor(mlp_hidden_sizes, num_output, pretrained_model_name=clip_model_name, lr=learning_rate, device=device, freeze_pretrained_model=False)
 model = model.to(device)
 
-print(f'learning_rate: {learning_rate}, mlp_hidden_sizes: {mlp_hidden_sizes}, jump_frames: {jump_frames},  add_penalty_loss: {add_penalty_loss}')
+print(f'learning_rate: {learning_rate}, mlp_hidden_sizes: {mlp_hidden_sizes}, jump_frames: {jump_frames},  use_reconstruction_layer: {use_reconstruction_layer}')
 model.train_model(train_loader, val_loader, num_epochs=num_epochs)
 
 
