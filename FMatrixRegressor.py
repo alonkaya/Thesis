4823625d84a8ba6be601dd29ac4ddd99f9b0d40f
alonkaya <alon.kaya.1@gmail.com>
2024-01-23 10:04:42 +0200

    .

--- FMatrixRegressor.py
+++ FMatrixRegressor.py
@@ -133,6 +133,7 @@ class FMatrixRegressor(nn.Module):
             train_size = 0
             for first_image, second_image, label, unormalized_label in train_loader:
                 if first_image is 0: 
+                    print("0")
                     continue
 
                 first_image, second_image, label = first_image.to(self.device), second_image.to(self.device), label.to(self.device) 
