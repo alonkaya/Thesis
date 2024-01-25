30299c55a2702c6915ce8eaac97b6181a3b18d5e
alonkaya <alon.kaya.1@gmail.com>
2024-01-25 09:19:40 +0200

    utilize gpu

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from FMatrixRegressor import FMatrixRegressor
 from params import *
 from Dataset import get_data_loaders
 
-model = FMatrixRegressor(mlp_hidden_sizes, num_output, pretrained_model_name=clip_model_name, lr=learning_rate, device=device, freeze_pretrained_model=False)
+model = FMatrixRegressor(mlp_hidden_sizes, num_output, pretrained_model_name=clip_model_name, lr=learning_rate, freeze_pretrained_model=False)
 model = model.to(device)
 
 train_loader, val_loader = get_data_loaders()
