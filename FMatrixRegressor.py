cda5346f699d6b166a6aa63f8643a476b185c996
alonkaya <alon.kaya.1@gmail.com>
2024-01-25 19:54:51 +0200

    utilize gpu

--- FMatrixRegressor.py
+++ FMatrixRegressor.py
@@ -264,10 +264,10 @@ class FMatrixRegressor(nn.Module):
 
 def get_avg_epipolar_test_errors(first_image, second_image, unormalized_label, output, unormalized_output):
     # Compute mean epipolar constraint error 
-    avg_ec_err_truth += EpipolarGeometry(first_image.detach(), second_image.detach(), unormalized_label.detach()).get_epipolar_err()
-    avg_ec_err_pred += EpipolarGeometry(first_image.detach(), second_image.detach(), output.detach()).get_epipolar_err()
-    avg_ec_err_pred_unormalized += EpipolarGeometry(first_image.detach(), second_image.detach(), unormalized_output.detach()).get_epipolar_err()
-    
+    avg_ec_err_truth = EpipolarGeometry(first_image.detach(), second_image.detach(), unormalized_label.detach()).get_epipolar_err()
+    avg_ec_err_pred = EpipolarGeometry(first_image.detach(), second_image.detach(), output.detach()).get_epipolar_err()
+    avg_ec_err_pred_unormalized = EpipolarGeometry(first_image.detach(), second_image.detach(), unormalized_output.detach()).get_epipolar_err()
+
     # for img_1, img_2, F_truth, F_pred, F_pred_unormalized in zip(first_image, second_image, unormalized_label, output, unormalized_output):
     #     avg_ec_err_truth += EpipolarGeometry(img_1.detach(), img_2.detach(), F_truth.detach()).get_epipolar_err()
     #     avg_ec_err_pred += EpipolarGeometry(img_1.detach(), img_2.detach(), F_pred.detach()).get_epipolar_err()
