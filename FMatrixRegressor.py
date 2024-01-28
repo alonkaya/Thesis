e89036f957ef80f145d5523240ec384476dbab89
alonkaya <alon.kaya.1@gmail.com>
2024-01-28 20:30:26 +0200

    .

--- FMatrixRegressor.py
+++ FMatrixRegressor.py
@@ -134,9 +134,9 @@ class FMatrixRegressor(nn.Module):
                 self.optimizer.step()
 
                 # Compute train mean epipolar constraint error
-                avg_ec_err_truth, avg_ec_err_pred, avg_ec_err_pred_unormalized = get_avg_epipolar_test_errors(
-                    first_image.detach(), second_image.detach(), unormalized_label.detach(), output.detach(), unnormalized_output.detach())
-                # avg_ec_err_truth, avg_ec_err_pred, avg_ec_err_pred_unormalized = torch.tensor(0).to(device), torch.tensor(0).to(device), torch.tensor(0).to(device)
+                # avg_ec_err_truth, avg_ec_err_pred, avg_ec_err_pred_unormalized = get_avg_epipolar_test_errors(
+                    # first_image.detach(), second_image.detach(), unormalized_label.detach(), output.detach(), unnormalized_output.detach())
+                avg_ec_err_truth, avg_ec_err_pred, avg_ec_err_pred_unormalized = torch.tensor(0).to(device), torch.tensor(0).to(device), torch.tensor(0).to(device)
                 epoch_avg_ec_err_truth += avg_ec_err_truth
                 epoch_avg_ec_err_pred += avg_ec_err_pred
                 epoch_avg_ec_err_pred_unormalized += avg_ec_err_pred_unormalized
@@ -179,9 +179,9 @@ class FMatrixRegressor(nn.Module):
                     val_avg_loss += self.L2_loss(val_output, val_label)
 
                     # Compute val mean epipolar constraint error
-                    val_avg_ec_err_truth, val_avg_ec_err_pred, val_avg_ec_err_pred_unormalized = get_avg_epipolar_test_errors(
-                        val_first_image, val_second_image, val_unormalized_label, val_output, unnormalized_val_output)
-                    # val_avg_ec_err_truth, val_avg_ec_err_pred, val_avg_ec_err_pred_unormalized = torch.tensor(0).to(device), torch.tensor(0).to(device), torch.tensor(0).to(device)
+                    # val_avg_ec_err_truth, val_avg_ec_err_pred, val_avg_ec_err_pred_unormalized = get_avg_epipolar_test_errors(
+                    #     val_first_image, val_second_image, val_unormalized_label, val_output, unnormalized_val_output)
+                    val_avg_ec_err_truth, val_avg_ec_err_pred, val_avg_ec_err_pred_unormalized = torch.tensor(0).to(device), torch.tensor(0).to(device), torch.tensor(0).to(device)
                     val_epoch_avg_ec_err_truth += val_avg_ec_err_truth
                     val_epoch_avg_ec_err_pred += val_avg_ec_err_pred
                     val_epoch_avg_ec_err_pred_unormalized += val_avg_ec_err_pred_unormalized
