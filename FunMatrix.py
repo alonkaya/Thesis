1fa527978aba3034e374da027bcc7b27f809c619
alonkaya <alon.kaya.1@gmail.com>
2024-02-06 20:12:28 +0200

    .

--- FunMatrix.py
+++ FunMatrix.py
@@ -64,7 +64,7 @@ def compute_essential(R, t):
     # Compute the skew-symmetric matrix of t
     t_x = torch.tensor([[0, -t[2], t[1]],
                         [t[2], 0, -t[0]],
-                        [-t[1], t[0], 0]])
+                        [-t[1], t[0], 0]]).to(device)
 
     # Compute the essential matrix E
     E = torch.matmul(t_x, R)
