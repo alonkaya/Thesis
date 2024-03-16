d6ec2192edd78d35d47bcdf281fe4c6879cd83f7
alonkaya <alon.kaya.1@gmail.com>
2024-03-16 10:26:59 +0200

    predicting F from pose

--- params.py
+++ params.py
@@ -23,7 +23,7 @@ NUM_WORKERS = 0
 BN_AND_DO = True if BATCH_SIZE > 1 else False
 CUSTOMDATASET_TYPE = "CustomDataset_first_two_thirds_train"
 
-penalty_coeffs = [1]
+penalty_coeffs = [0]
 RE1_COEFF = 0
 ENFORCE_RANK_2 = False
 USE_RECONSTRUCTION_LAYER = False
