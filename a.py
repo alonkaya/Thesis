8bb73e3b3250157dfb918bd960e409c651445ce3
alonkaya <alon.kaya.1@gmail.com>
2024-03-30 17:11:20 +0300

    .

--- a.py
+++ a.py
@@ -35,46 +35,48 @@ class CustomDataset_first_two_thirds_train(torch.utils.data.Dataset):
             else:
                 idx = self.valid_indices[idx + ((len(self.valid_indices) - JUMP_FRAMES) // 3) * 2]
 
-            original_first_image = Image.open(os.path.join(self.sequence_path, f'{idx:06}.{IMAGE_TYPE}'))
-            original_second_image = Image.open(os.path.join(self.sequence_path, f'{idx+JUMP_FRAMES:06}.{IMAGE_TYPE}'))
+            with Image.open(os.path.join(self.sequence_path, f'{idx:06}.{IMAGE_TYPE}')) as original_first_image:
+                with Image.open(os.path.join(self.sequence_path, f'{idx+JUMP_FRAMES:06}.{IMAGE_TYPE}')) as original_second_image:
+                    first_image = self.transform(original_first_image)
+                    second_image = self.transform(original_second_image)    
+
+                    try:
+                        # Transform: Resize, center, grayscale
+                        first_image = transform2(original_first_image)
+                        second_image = transform2(original_second_image)
+                        # first_image = self.transform(original_first_image)
+                        # second_image = self.transform(original_second_image)
+                    except Exception as e:
+                        print_and_write(f"2\nError in sequence: {self.sequence_path}, idx: {idx}, dataset_type: {self.dataset_type} sequence num: {self.sequence_num}\nException: {e}")
+                    
+                    try:
+                        first_image = first_image
+                        second_image = second_image
+                    except Exception as e:
+                        print_and_write(f"3\nError in sequence: {self.sequence_path}, idx: {idx}, dataset_type: {self.dataset_type} sequence num: {self.sequence_num}\nException: {e}")
+                    
+                    try:
+                        if PREDICT_POSE:
+                            unormalized_R, unormalized_t = compute_relative_transformations(self.poses[idx],self. poses[idx+JUMP_FRAMES])
+                            unormalized_label = torch.cat((unormalized_R, unormalized_t.view(3,1)), dim=-1)
+                        else:
+                            unormalized_label = get_F(self.poses, idx, self.k)
+                    except Exception as e:
+                        print_and_write(f"4\nError in sequence: {self.sequence_path}, idx: {idx}, dataset_type: {self.dataset_type} sequence num: {self.sequence_num}\nException: {e}")
+                    try:
+                        if PREDICT_POSE:
+                            R, t = norm_layer(unormalized_R.view(-1, 9)).view(3,3), norm_layer(unormalized_t.view(-1, 3), predict_t=True).view(3)
+
+                            label = torch.cat((R, t.view(3,1)), dim=-1)
+                        else:               
+                            label = norm_layer(unormalized_label.view(-1, 9)).view(3,3)
+                    except Exception as e:
+                        print_and_write("5\n {e}")
+                        
+                    return first_image, second_image, label, unormalized_label, self.k
         except Exception as e:
             print_and_write(f"1\nError in sequence: {self.sequence_path}, idx: {idx}, dataset_type: {self.dataset_type} sequence num: {self.sequence_num}\nException: {e}")
         
-        try:
-            # Transform: Resize, center, grayscale
-            first_image = transform2(original_first_image)
-            second_image = transform2(original_second_image)
-            # first_image = self.transform(original_first_image)
-            # second_image = self.transform(original_second_image)
-        except Exception as e:
-            print_and_write(f"2\nError in sequence: {self.sequence_path}, idx: {idx}, dataset_type: {self.dataset_type} sequence num: {self.sequence_num}\nException: {e}")
-        
-        try:
-            first_image = first_image
-            second_image = second_image
-        except Exception as e:
-            print_and_write(f"3\nError in sequence: {self.sequence_path}, idx: {idx}, dataset_type: {self.dataset_type} sequence num: {self.sequence_num}\nException: {e}")
-        
-        try:
-            if PREDICT_POSE:
-                unormalized_R, unormalized_t = compute_relative_transformations(self.poses[idx],self. poses[idx+JUMP_FRAMES])
-                unormalized_label = torch.cat((unormalized_R, unormalized_t.view(3,1)), dim=-1)
-            else:
-                unormalized_label = get_F(self.poses, idx, self.k)
-        except Exception as e:
-            print_and_write(f"4\nError in sequence: {self.sequence_path}, idx: {idx}, dataset_type: {self.dataset_type} sequence num: {self.sequence_num}\nException: {e}")
-        try:
-            if PREDICT_POSE:
-                R, t = norm_layer(unormalized_R.view(-1, 9)).view(3,3), norm_layer(unormalized_t.view(-1, 3), predict_t=True).view(3)
-
-                label = torch.cat((R, t.view(3,1)), dim=-1)
-            else:               
-                label = norm_layer(unormalized_label.view(-1, 9)).view(3,3)
-        except Exception as e:
-            print_and_write("5\n {e}")
-            
-        return first_image, second_image, label, unormalized_label, self.k
-    
 
 def transform2(img):
     # Resize the image
