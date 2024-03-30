034d9a00408f13e4d67d54de9ab3afd856693add
alonkaya <alon.kaya.1@gmail.com>
2024-03-30 18:29:08 +0300

    .

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
+from utils import print_and_write, init_main
+init_main()
+
 from FMatrixRegressor import FMatrixRegressor
 from params import *
-from utils import print_and_write, init_main
 from Dataset import *
 import itertools
 from a import * 
 
 if __name__ == "__main__":
-    init_main()
+    # init_main()
         
     # Iterate over each combination
     param_combinations = itertools.product(penalty_coeffs, penaltize_normalized_options, learning_rates_vit, learning_rates_mlp)
