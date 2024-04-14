c395b52e7178834d856bbd346b37d476b7022adc
alonkaya <alon.kaya.1@gmail.com>
2024-04-14 20:51:53 +0300

    .

--- PlotFromText.py
+++ PlotFromText.py
@@ -93,9 +93,9 @@ def plot_parameter(x, y1, y2, title, plots_path=None, x_label="Epochs", save=Fal
 
 
 if __name__ == "__main__":
-    plots_path = "plots/RealEstate/SVD_0__RE1_0__SED_0.01__ALG_0__lr_2e-05__avg_embeddings_True__model_CLIP__use_reconstruction_True__Augmentation_False__Conv_True"
+    plots_path = "plots/RealEstate/SVD_0__RE1_0.01__SED_0__ALG_0__lr_2e-05__avg_embeddings_True__model_CLIP__use_reconstruction_True__Augmentation_True__Conv_False"
     file_path = os.path.join(plots_path, "output.log")
-    save = True
+    save = False
 
     process_epoch_stats(file_path)
     print(len(epochs), len(training_losses), len(val_losses), len(training_maes), len(val_maes), len(alg_dists), len(val_alg_dists), len(re1_dists), len(val_re1_dists), len(sed_dists), len(val_sed_dists))
