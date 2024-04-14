bf07cdf033d3d6eda78397b0a1dda0a676dfded6
alonkaya <alon.kaya.1@gmail.com>
2024-04-14 20:26:51 +0300

    .

--- params.py
+++ params.py
@@ -1,9 +1,9 @@
 import torch
 
 DEVICE_ID = 1
-if DEVICE_ID==0: DEVICE_ID=1
-elif DEVICE_ID==2: DEVICE_ID=1
-elif DEVICE_ID==1: DEVICE_ID==0
+# if DEVICE_ID==0: DEVICE_ID=1
+# elif DEVICE_ID==2: DEVICE_ID=1
+# elif DEVICE_ID==1: DEVICE_ID==0
 device = torch.device(f"cuda:{DEVICE_ID}" if torch.cuda.is_available() else "cpu")
 
 learning_rates_vit = [2e-5]
