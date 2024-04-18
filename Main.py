ee652cc92e6e1e8679d58399bc64d61442e983a4
alonkaya <alon.kaya.1@gmail.com>
2024-04-18 21:06:34 +0300

    .

--- Main.py
+++ Main.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         coeff = f'ALG_sqr_{alg_coeff}__' if alg_coeff > 0 else f'RE1_{re1_coeff}__' if re1_coeff > 0 else f'SED_{sed_coeff}__' if sed_coeff > 0 else ''
         dataset = "__first_2_thirds_train" if FIRST_2_THRIDS_TRAIN else "__first_2_of_three_train" if FIRST_2_OF_3_TRAIN else ""
         plots_path = os.path.join('plots', 'RealEstate', 
-                          f"""{coeff}__coeffs__lr_{learning_rates_mlp[0]}__\
+                          f"""{coeff}{ADDITIONS}lr_{learning_rates_mlp[0]}__\
 avg_embeddings_{AVG_EMBEDDINGS}__model_{"CLIP" if MODEL == CLIP_MODEL_NAME else "Google ViT"}__\
 use_reconstruction_{USE_RECONSTRUCTION_LAYER}__Augmentation_{AUGMENTATION}__Conv_{GROUP_CONV["use"]}{dataset}""")\
         
@@ -37,7 +37,7 @@ learning rate vit: {lr_vit}, learning rate mlp: {lr_mlp}, mlp_hidden_sizes: {MLP
 batch_size: {BATCH_SIZE}, train_seqeunces: {train_seqeunces}, val_sequences: {val_sequences}, RealEstate: {USE_REALESTATE}, batchnorm & dropout: {BN_AND_DO}, 
 average embeddings: {AVG_EMBEDDINGS}, model: {MODEL}, augmentation: {AUGMENTATION}, 
 predict pose: {PREDICT_POSE}, SVD coeff: {LAST_SV_COEFF}, RE1 coeff: {re1_coeff} SED coeff: {sed_coeff}, ALG_COEFF: {alg_coeff}, unforzen layers: {UNFROZEN_LAYERS}, group conv: {GROUP_CONV["use"]}
-Dataset: {dataset}\n\n"""
+Dataset: {dataset} {ADDITIONS}\n\n"""
         print_and_write(parameters, plots_path)
 
         model.train_model(train_loader, val_loader, num_epochs=NUM_EPOCHS)
