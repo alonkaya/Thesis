777b2d377f902729476d860a24504d4031fb2c9b
alonkaya <alon.kaya.1@gmail.com>
2024-04-26 09:31:21 +0300

    .

--- DatasetOneSequence.py
+++ DatasetOneSequence.py
@@ -134,7 +134,7 @@ def worker_init_fn(worker_id):
     sys.excepthook = worker_exception_handler
 
 
-def data_with_one_sequence(batch_size, sequence_name='f1ee9dc6135e5307'):
+def data_with_one_sequence(batch_size, sequence_name='30b1d229ad4c6353'):
     RealEstate_path = 'RealEstate10K/val_images'
     # sequence_name = '0cb8672999a42a05'
     # sequence_name = "0000cc6d8b108390"
