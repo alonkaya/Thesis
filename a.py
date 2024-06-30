8c4d499a914b10e3a65f432c46b4cd33cbef3db3
alonkaya <alon.kaya.1@gmail.com>
2024-06-30 10:32:27 +0300

    >

--- a.py
+++ a.py
@@ -1,11 +1,12 @@
 import cv2
+import numpy as np
+
 # Function to draw points on the image
-def draw_points(image, points, color=(0, 255, 0)):
-    for point in points:
-        if point[0] == 0 and point[1] == 0: continue
-        print(int(point[0]), int(point[1]))
-        print(image)
-        cv2.circle(image, (int(point[0]), int(point[1])), 5, color, -1)
+def draw_points(image, point, color=(0, 255, 0)):
+    # for point in points:
+    print(int(point[0]), int(point[1]))
+    print(image)
+    cv2.circle(image, (int(point[0]), int(point[1])), 5, color, -1)
 
 # File path to the image
 file_path = "sequences/00/image_0/004134.png"
@@ -13,5 +14,5 @@ file_path = "sequences/00/image_0/004134.png"
 # Read the image using OpenCV
 image_np = cv2.imread(file_path, cv2.IMREAD_COLOR)
 # Draw points on the images
-draw_points(image_np, 47)
-draw_points(image_np, 46)
+points = np.array(47,46)
+draw_points(image_np, points)
