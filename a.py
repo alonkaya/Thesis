fb6f2503b60023dae09fc41df3767b5a5b5f5263
alonkaya <alon.kaya.1@gmail.com>
2024-06-30 09:55:38 +0300

    >

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('test_image.jpg')  # Use a valid image file path
+img = cv2.imread("epipole_lines/bad_frames/epipoLine_sift_39.png")  # Use a valid image file path
 cv2.imshow('Test Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
