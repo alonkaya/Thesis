4697e2896683b7360fc57712a5a1dd45957defab
alonkaya <alon.kaya.1@gmail.com>
2024-06-30 10:34:58 +0300

    >

--- PlayGround.py
+++ PlayGround.py
@@ -111,10 +111,9 @@ def vis_gt():
         def draw_points(image, points, color=(0, 255, 0)):
             for point in points:
                 if point[0] == 0 and point[1] == 0: continue
-                print(int(point[0]), int(point[1]))
-                print(image)
+                print(image.shape)
                 cv2.circle(image, (int(point[0]), int(point[1])), 5, color, -1)
-
+        
         # Draw points on the images
         draw_points(img0_np, pts1.cpu().numpy())
         draw_points(img1_np, pts2.cpu().numpy())
