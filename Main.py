f2447e5a0a23518cd4e065e760fbfcc3ffbcc651
alonkaya <alon.kaya.1@gmail.com>
2024-07-29 21:00:22 +0300

    .

--- Main.py
+++ Main.py
@@ -36,7 +36,6 @@ if __name__ == "__main__":
                 not_good = f.read().splitlines()
 
         for i, (alg_coeff, re1_coeff, sed_coeff, data_ratio, lr, bs) in enumerate(param_combinations):
-                if i < 5: continue
                 lr_decay = 0.85 if lr < 1e-4 else 0.8
                 num_epochs = 1200 if data_ratio==0.3 else 2000 if data_ratio==0.2 else 3500 if data_ratio==0.1 else 4000 if data_ratio==0.05 else 0
 
@@ -54,28 +53,27 @@ if __name__ == "__main__":
 use_reconstruction_{USE_RECONSTRUCTION_LAYER}__BS_{bs}{dataset_class}__ratio_{data_ratio}__head_{HEAD}""")
                 
                 if plots_path in not_good:
-                        print(f"Skipping {plots_path}")
                         continue
-                print(plots_path)
-                # train_loader, val_loader, test_loader = get_data_loaders(data_ratio, bs)
-                # try:
-                #         model = FMatrixRegressor(lr=lr, lr_decay=lr_decay, min_lr=MIN_LR, batch_size=bs, L2_coeff=L2_coeff, huber_coeff=huber_coeff, alg_coeff=alg_coeff, re1_coeff=re1_coeff, sed_coeff=sed_coeff, plots_path=plots_path, pretrained_path=PRETRAINED_PATH, num_epochs=num_epochs).to(device)
-                # except Exception as e:
-                #         print(f"\n {plots_path}")
-                #         continue
-                # if model.start_epoch < model.num_epochs:
-                #         if not PRETRAINED_PATH and not os.path.exists(os.path.join(plots_path, 'model.pth')):
-                #                 parameters = f"""###########################################################################################################################################################\n
-                #         {ADDITIONS} learning rate: {lr}, lr_decay: {lr_decay}, mlp_hidden_sizes: {MLP_HIDDEN_DIM}, jump_frames: {JUMP_FRAMES}, use_reconstruction_layer: {USE_RECONSTRUCTION_LAYER}
-                #         batch_size: {bs}, norm: {NORM}, train_seqeunces: {train_seqeunces_stereo if STEREO else train_seqeunces}, val_sequences: {val_sequences_stereo if STEREO else val_sequences}, dataset: {dataset},
-                #         average embeddings: {AVG_EMBEDDINGS}, model: {MODEL}, augmentation: {AUGMENTATION}, random crop: {RANDOM_CROP}, deepF_nocorrs: {DEEPF_NOCORRS}, head: {HEAD},
-                #         SVD coeff: {LAST_SV_COEFF}, RE1 coeff: {re1_coeff} SED coeff: {sed_coeff}, ALG_COEFF: {alg_coeff}, L2_coeff: {L2_coeff}, huber_coeff: {huber_coeff}, unforzen layers: {UNFROZEN_LAYERS}, group conv: {GROUP_CONV["use"]}
-                #         crop: {CROP} resize: {RESIZE}, use conv: {USE_CONV} pretrained: {PRETRAINED_PATH}, data_ratio: {data_ratio}, norm_mean: {norm_mean}, norm_std: {norm_std}, sched: {SCHED}, \n\n"""
-                #                 print_and_write(parameters, model.plots_path)
-                #         else:
-                #                 print_and_write(f"##### CONTINUE TRAINING #####\n\n", model.plots_path)
+
+                train_loader, val_loader, test_loader = get_data_loaders(data_ratio, bs)
+                try:
+                        model = FMatrixRegressor(lr=lr, lr_decay=lr_decay, min_lr=MIN_LR, batch_size=bs, L2_coeff=L2_coeff, huber_coeff=huber_coeff, alg_coeff=alg_coeff, re1_coeff=re1_coeff, sed_coeff=sed_coeff, plots_path=plots_path, pretrained_path=PRETRAINED_PATH, num_epochs=num_epochs).to(device)
+                except Exception as e:
+                        print(f"\n {plots_path}")
+                        continue
+                if model.start_epoch < model.num_epochs:
+                        if not PRETRAINED_PATH and not os.path.exists(os.path.join(plots_path, 'model.pth')):
+                                parameters = f"""###########################################################################################################################################################\n
+                        {ADDITIONS} learning rate: {lr}, lr_decay: {lr_decay}, mlp_hidden_sizes: {MLP_HIDDEN_DIM}, jump_frames: {JUMP_FRAMES}, use_reconstruction_layer: {USE_RECONSTRUCTION_LAYER}
+                        batch_size: {bs}, norm: {NORM}, train_seqeunces: {train_seqeunces_stereo if STEREO else train_seqeunces}, val_sequences: {val_sequences_stereo if STEREO else val_sequences}, dataset: {dataset},
+                        average embeddings: {AVG_EMBEDDINGS}, model: {MODEL}, augmentation: {AUGMENTATION}, random crop: {RANDOM_CROP}, deepF_nocorrs: {DEEPF_NOCORRS}, head: {HEAD},
+                        SVD coeff: {LAST_SV_COEFF}, RE1 coeff: {re1_coeff} SED coeff: {sed_coeff}, ALG_COEFF: {alg_coeff}, L2_coeff: {L2_coeff}, huber_coeff: {huber_coeff}, unforzen layers: {UNFROZEN_LAYERS}, group conv: {GROUP_CONV["use"]}
+                        crop: {CROP} resize: {RESIZE}, use conv: {USE_CONV} pretrained: {PRETRAINED_PATH}, data_ratio: {data_ratio}, norm_mean: {norm_mean}, norm_std: {norm_std}, sched: {SCHED}, \n\n"""
+                                print_and_write(parameters, model.plots_path)
+                        else:
+                                print_and_write(f"##### CONTINUE TRAINING #####\n\n", model.plots_path)
                 
-                #         model.train_model(train_loader, val_loader, test_loader)
+                        model.train_model(train_loader, val_loader, test_loader)
 
                 torch.cuda.empty_cache()
 
