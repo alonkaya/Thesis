4b6c8d20b466a6b2d964989d4ac3cb208a7ad8f8
alonkaya <alon.kaya.1@gmail.com>
2024-08-09 14:23:09 +0300

    >

--- params.py
+++ params.py
@@ -69,7 +69,7 @@ USE_CONV = True
 USE_CONV = False if DEEPF_NOCORRS else USE_CONV
 GROUP_CONV = {"use" : False, "out_channels": 256}
 NUM_OUTPUT = 8 if USE_RECONSTRUCTION_LAYER else 9
-FROZEN_LAYERS = 4
+FROZEN_LAYERS = 8
 norm_mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
 norm_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
 
