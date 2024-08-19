6eb96aedc1f37029b7e0f749f2660827d41ea7d1
alonkaya <alon.kaya.1@gmail.com>
2024-08-19 12:15:31 +0300

    .

--- params.py
+++ params.py
@@ -11,7 +11,7 @@ FIRST_2_OF_3_TRAIN = False
 train_seqeunces_stereo = [0,2,3,5] #  10840 images 
 val_sequences_stereo =  [6,7,8]    #  3682 images
 test_sequences_stereo = [9]        #  1064 images
-seq_ratios = [0.0375]      # 3251, 2166, 1082, 540, 405, 269
+seq_ratios = [0.5]      # 3251, 2166, 1082, 540, 405, 269
 CROP = 224
 RESIZE = 256
 USE_REALESTATE = False
@@ -29,7 +29,7 @@ WEIGHT_DECAY = 0                                                              # 
 MIN_LR = 2e-5
 SCHED = None
 USE_RECONSTRUCTION_LAYER = True
-BATCH_SIZE = [8]                                                         # TODO 16, 32, 64
+BATCH_SIZE = [4]                                                         # TODO 16, 32, 64
 NORM = True
 TRAIN_FROM_SCRATCH = False
 DEEPF_NOCORRS = False
@@ -68,7 +68,7 @@ AVG_EMBEDDINGS = False
 USE_CONV = True
 USE_CONV = False if DEEPF_NOCORRS else USE_CONV
 NUM_OUTPUT = 8 if USE_RECONSTRUCTION_LAYER else 9
-FROZEN_LAYERS = [0]
+FROZEN_LAYERS = [4, 8]
 norm_mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
 norm_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
 
