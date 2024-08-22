625ab0d00d5fcbbd11ff78321da4ad7491710014
alonkaya <alon.kaya.1@gmail.com>
2024-08-22 19:50:33 +0300

    >

--- params.py
+++ params.py
@@ -11,7 +11,7 @@ FIRST_2_OF_3_TRAIN = False
 train_seqeunces_stereo = [0,2,3,5] #  10840 images 
 val_sequences_stereo =  [6,7,8]    #  3682 images
 test_sequences_stereo = [9]        #  1064 images
-SEQ_RATIOS = [0.0375]      # 3251, 2166, 1082, 540, 405, 269
+SEQ_RATIOS = [0.025]      # 3251, 2166, 1082, 540, 405, 269
 CROP = 224
 RESIZE = 256
 USE_REALESTATE = False
@@ -21,7 +21,7 @@ JUMP_FRAMES = 6 if USE_REALESTATE else 2
 AUGMENTATION = True
 RANDOM_CROP = True
 INIT_DATA = True
-PART = ["tail"]                                                  
+PART = ["mid"]                                                  
 
 ### Training ###
 LR = [1e-4]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
@@ -61,7 +61,7 @@ CONV_HIDDEN_DIM = [256, 512]
 CLIP_MODEL_NAME = "openai/clip-vit-base-patch32"
 # CLIP_MODEL_NAME = "openai/clip-vit-large-patch14"
 VIT_MODEL_NAME = "google/vit-base-patch32-224-in21k"
-PRETRAINED_PATH =  "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__auged__2lr_0.0001__conv__CLIP__use_reconstruction_True__BS_8__ratio_0.0375__tail__frozen_0"
+PRETRAINED_PATH =  "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__lr_0.0001__conv__CLIP__use_reconstruction_True/BS_8__ratio_0.025__mid__frozen_0"
 RESNET_MODEL_NAME = 'microsoft/resnet-152'
 MODEL = CLIP_MODEL_NAME
 FREEZE_PRETRAINED_MODEL=False
@@ -74,7 +74,7 @@ norm_mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device) if MODE
 norm_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
 
 
-# "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__auged__2lr_0.0001__conv__CLIP__use_reconstruction_True__BS_8__ratio_0.0375__tail__frozen_0"
+# currently on 3080 "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__auged__2lr_0.0001__conv__CLIP__use_reconstruction_True__BS_8__ratio_0.0375__tail__frozen_0"
 # "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__lr_0.0001__conv__CLIP__use_reconstruction_True/BS_8__ratio_0.025__mid__frozen_0"
 # "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__lr_0.0001__conv__CLIP__use_reconstruction_True/BS_8__ratio_0.025__tail__frozen_0"
 # waiting on 4090 "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__auged__lr_0.0001__conv__CLIP__use_reconstruction_True__BS_4__ratio_0.025__head_True"
