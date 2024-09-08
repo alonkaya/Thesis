b23177f986931d1f31075ad7e2a60996a004033d
alonkaya <alon.kaya.1@gmail.com>
2024-09-08 18:52:19 +0300

    .

--- AffineRegressor.py
+++ AffineRegressor.py
@@ -245,7 +245,7 @@ class AffineRegressor(nn.Module):
         self.hidden_size = self.model.config.hidden_size if not self.resnet else self.model.config.hidden_sizes[-1]
         self.num_patches = self.model.config.image_size // self.model.config.patch_size if not self.resnet else 7
         mlp_input_shape = 2 * (self.num_patches**2) * self.hidden_size 
-
+        print(mlp_input_shape)
         # Initialize loss functions
         self.L2_loss = nn.MSELoss().to(device)
         self.huber_loss = nn.HuberLoss().to(device)
