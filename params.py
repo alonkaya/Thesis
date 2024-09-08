50b9da0b1f8047d63d22586420f07176317f6e22
alonkaya <alon.kaya.1@gmail.com>
2024-09-08 16:12:04 +0300

    .

--- params.py
+++ params.py
@@ -8,9 +8,9 @@ CROP = 224
 RESIZE = 256
 ANGLE_RANGE = 90
 SHIFT_RANGE = 100
-train_length = 2048 # Needs to be a multiple of batch size
-val_length = 320 # Needs to be a multiple of batch size
-test_length = 320 # Needs to be a multiple of batch size
+train_length = 1024 # Needs to be a multiple of batch size
+val_length = 160 # Needs to be a multiple of batch size
+test_length = 160 # Needs to be a multiple of batch size
 
 ### Training ###
 LR = [1e-4, 4e-5, 5e-4]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
