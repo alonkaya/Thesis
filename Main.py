9c4a8a3aff37355864f33d189377220c1ed45bb4
alonkaya <alon.kaya.1@gmail.com>
2024-09-08 15:10:32 +0300

    .

--- Main.py
+++ Main.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
                 if model.start_epoch < model.num_epochs:
                         parameters = f"""###########################################################################################################################################################\n
-                        {ADDITIONS} learning rate: {lr},  mlp_hidden_sizes: {MLP_HIDDEN_DIM}, batch_size: {bs}, norm: {NORM},
+                        {ADDITIONS} learning rate: {lr},  mlp_hidden_sizes: {MLP_HIDDEN_DIM}, batch_size: {bs}, norm: {NORM}, alpha: {alpha},
                         crop: {CROP} resize: {RESIZE}, use conv: {USE_CONV} pretrained: {PRETRAINED_PATH}, seed: {SEED}, angle range: {ANGLE_RANGE}, shift range: {SHIFT_RANGE}, 
                         train length: {train_length}, val length: {val_length}, test length: {test_length}, get old path: {GET_OLD_PATH}\n\n\n"""
                         print_and_write(parameters, model.plots_path)
--- params.py
+++ params.py
@@ -13,7 +13,7 @@ val_length = 320 # Needs to be a multiple of batch size
 test_length = 320 # Needs to be a multiple of batch size
 
 ### Training ###
-LR = [1e-4, 4e-5, 5e-3]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
+LR = [4e-5, 1e-4]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
 BATCH_SIZE = [1]                                                                     # TODO 16, 32, 64
 NORM = True
 TRAIN_FROM_SCRATCH = False
