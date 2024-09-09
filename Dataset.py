80cc7a7dbba7a07e7f1e7b567a70228e33fc28b8
alonkaya <alon.kaya.1@gmail.com>
2024-09-09 11:59:47 +0300

    .

--- Dataset.py
+++ Dataset.py
@@ -36,7 +36,7 @@ class CustomDataset(torch.utils.data.Dataset):
         shift_y = 0 if self.shift_range==0 else torch.tensor(shift_y / self.shift_range, dtype=torch.float32)
 
         ret =  original_image, translated_image, angle if NUM_OUTPUT == 1 else original_image, translated_image, shift_x, shift_y if NUM_OUTPUT == 2 else original_image, translated_image, angle, shift_x, shift_y
-        print(ret)
+        print(NUM_OUTPUT)
         return ret
 
 
