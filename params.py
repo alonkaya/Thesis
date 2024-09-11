8e9f6022610870dad60f68cbd2c35271d89ce36f
alonkaya <alon.kaya.1@gmail.com>
2024-09-11 09:07:59 +0300

    .

--- params.py
+++ params.py
@@ -40,6 +40,6 @@ NUM_OUTPUT = 3
 mean = torch.tensor([0.485, 0.456, 0.406])
 std = torch.tensor([0.229, 0.224, 0.225])
 # only one of the following 3 should be True
-USE_CONV = True
-AVG_EMBEDDINGS = False
+USE_CONV = False
+AVG_EMBEDDINGS = True
 USE_CLS = [False]
