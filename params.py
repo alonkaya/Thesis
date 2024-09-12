df8ff04224b9144226e383906af42ee8c67799fb
alonkaya <alon.kaya.1@gmail.com>
2024-09-12 17:09:41 +0300

    .

--- params.py
+++ params.py
@@ -68,6 +68,6 @@ AVG_EMBEDDINGS = False
 USE_CONV = True
 USE_CONV = False
 NUM_OUTPUT = 8 if USE_RECONSTRUCTION_LAYER else 9
-FROZEN_LAYERS = [0]
+FROZEN_LAYERS = [0, 4]
 norm_mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
 norm_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
