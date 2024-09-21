ca09181da08a534197d61d045da71004ad8f0b6c
alonkaya <alon.kaya.1@gmail.com>
2024-09-21 10:13:34 +0300

    >

--- params.py
+++ params.py
@@ -2,6 +2,7 @@ import torch
 
 device = torch.device(f"cuda" if torch.cuda.is_available() else "cpu")
 # 977733 is output_resnet_0.025_0.0375_0.05.log
+# output resnet is SEQ_RATIOS = [0.1, 0.2]      
 
 ### Dataset ###
 train_seqeunces = [0, 2, 3, 5, 6, 7, 8]
@@ -11,7 +12,7 @@ FIRST_2_OF_3_TRAIN = False
 train_seqeunces_stereo = [0,2,3,5] #  10840 images 
 val_sequences_stereo =  [6,7,8]    #  3682 images
 test_sequences_stereo = [9]        #  1064 images
-SEQ_RATIOS = [0.1, 0.2]      # 3251, 2166, 1082, 540, 405, 269
+SEQ_RATIOS = [0.0375]      # 3251, 2166, 1082, 540, 405, 269
 CROP = 224
 RESIZE = 256
 USE_REALESTATE = False
@@ -21,10 +22,10 @@ JUMP_FRAMES = 6 if USE_REALESTATE else 2
 AUGMENTATION = True
 RANDOM_CROP = True
 INIT_DATA = True
-PART = ["head", "mid", "tail"]                                                  
+PART = ["tail"]                                                  
 
 ### Training ###
-LR = [1e-4]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
+LR = [4e-4]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
 WEIGHT_DECAY = 0                                                          # TODO 5e-4, 5e-5
 MIN_LR = 2e-5
 SCHED = None
@@ -38,7 +39,7 @@ NUM_WORKERS = 0
 SAVE_MODEL = True
 NUM_EPOCHS = 1500
 GET_OLD_PATH = False
-SEED = [42, 300]
+SEED = [400]
 
 ### Epipolar geometry ###
 RE1_DIST = True
@@ -61,9 +62,9 @@ CLIP_MODEL_NAME = "openai/clip-vit-base-patch32"
 # CLIP_MODEL_NAME = "openai/clip-vit-large-patch14"
 VIT_MODEL_NAME = "google/vit-base-patch32-224-in21k"
 PRETRAINED_PATH =  None # make sure you set GET_OLD_PATH !! 
-TRAINED_VIT =  None# "plots/Affine/BS_32__lr_6e-05__train_size_9216__CLIP__alpha_10__conv__original_rotated/model.pth" # This is for when wanting to fine-tune an already trained vit (for example fine-tuning a vit which had been trained on the affine transfomration task)
+TRAINED_VIT =  "plots/Affine/BS_32__lr_6e-05__train_size_9216__CLIP__alpha_10__conv__original_rotated/model.pth" # This is for when wanting to fine-tune an already trained vit (for example fine-tuning a vit which had been trained on the affine transfomration task)
 RESNET_MODEL_NAME = 'microsoft/resnet-152'
-MODEL = RESNET_MODEL_NAME
+MODEL = CLIP_MODEL_NAME
 FREEZE_PRETRAINED_MODEL=False
 AVG_EMBEDDINGS = False
 USE_CONV = True
