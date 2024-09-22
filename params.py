af2f055595fac6b1af9685ed4cd76e5341db7eaf
alonkaya <alon.kaya.1@gmail.com>
2024-09-22 14:02:54 +0300

    >

--- params.py
+++ params.py
@@ -2,6 +2,7 @@ import torch
 
 device = torch.device(f"cuda" if torch.cuda.is_available() else "cpu")
 # 1179397 is output_resnet_0.025_0.0375_0.05.log
+# 1179721 is output_vit_trained_0.1_0.2.log
 
 ### Dataset ###
 train_seqeunces = [0, 2, 3, 5, 6, 7, 8]
@@ -11,7 +12,7 @@ FIRST_2_OF_3_TRAIN = False
 train_seqeunces_stereo = [0,2,3,5] #  10840 images 
 val_sequences_stereo =  [6,7,8]    #  3682 images
 test_sequences_stereo = [9]        #  1064 images
-SEQ_RATIOS = [0.1, 0.2]      # 3251, 2166, 1082, 540, 405, 269
+SEQ_RATIOS = [0.0375]      # 3251, 2166, 1082, 540, 405, 269
 CROP = 224
 RESIZE = 256
 USE_REALESTATE = False
@@ -21,10 +22,10 @@ JUMP_FRAMES = 6 if USE_REALESTATE else 2
 AUGMENTATION = True
 RANDOM_CROP = True
 INIT_DATA = True
-PART = ["head", "mid", "tail"]                                                  
+PART = ["tail"]                                                  
 
 ### Training ###
-LR = [1e-4]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
+LR = [4e-4]                                                               # TODO lr: 5e-4, 1e-4, 5e-5, 2e-5
 WEIGHT_DECAY = 0                                                          # TODO 5e-4, 5e-5
 MIN_LR = 2e-5
 SCHED = None
@@ -38,7 +39,7 @@ NUM_WORKERS = 0
 SAVE_MODEL = True
 NUM_EPOCHS = 1500
 GET_OLD_PATH = False
-SEED = [42, 300]
+SEED = [200, 700]
 
 ### Epipolar geometry ###
 RE1_DIST = True
@@ -69,6 +70,6 @@ AVG_EMBEDDINGS = False
 USE_CONV = True
 USE_CONV = False
 NUM_OUTPUT = 8 if USE_RECONSTRUCTION_LAYER else 9
-FROZEN_LAYERS = [0, 4, 8]
+FROZEN_LAYERS = [4]
 norm_mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
 norm_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device) if MODEL == CLIP_MODEL_NAME else torch.tensor([0.5, 0.5, 0.5]).to(device)
