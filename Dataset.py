a2fd501fb24eb6cf4b173cf1fa2537d27d5d6cd9
alonkaya <alon.kaya.1@gmail.com>
2024-09-23 19:26:13 +0300

    .

--- Dataset.py
+++ Dataset.py
@@ -184,7 +184,7 @@ def get_dataloaders_RealEstate(data_ratio, part, batch_size):
                 if len(custom_dataset) > 9:
                     if RealEstate_path == 'RealEstate10K/train_images':
                         train_datasets.append(custom_dataset) 
-                    elif sequence_name not in RealEstate_test_names:
+                    elif i < len(os.listdir(RealEstate_path))//2:
                         val_datasets.append(custom_dataset)
                     else:
                         test_datasets.append(custom_dataset)
