a6a731c5203bb1ad97a34e5395394104b507d79b
alonkaya <alon.kaya.1@gmail.com>
2024-09-24 12:54:29 +0300

    .

--- PlayGround.py
+++ PlayGround.py
@@ -133,50 +133,23 @@ def sed_distance_gt():
                    "test_loss": torch.tensor(0), "test_labels": torch.tensor([]), "test_outputs": torch.tensor([])}
     c=0
     for i, (img1, img2, label, pts1, pts2, seq_name, seq_path, idx) in enumerate(test_loader):
-        print(pts1.shape)
+        print(idx)
         if img1 == None:
             seq_path_parent = os.path.dirname(seq_path[0])
             source_path = os.path.join(seq_path[0], f'{idx[0]:06}.jpg')
             dest_path = os.path.join(seq_path_parent, "bad_frames", f'{idx[0]:06}.png')
-            print(f"Moving {source_path} to {dest_path}")
-            os.rename(source_path, dest_path)
+            print(c)
             c +=1
-
-    #     # update_epoch_stats(epoch_stats, img1.detach(), img2.detach(), label.detach(), label.detach(), pts1, pts2, "", data_type="test")
-    # print(c)
-    # print(len(test_loader))
-
-    # c=0
-    # for i, (img1, img2, label, pts1, pts2, seq_name, seq_path, idx) in enumerate(val_loader):
-    #     if img1 == None:
-    #         seq_path_parent = os.path.dirname(seq_path[0])
-    #         source_path = os.path.join(seq_path[0], f'{idx[0]:06}.jpg')
-    #         dest_path = os.path.join(seq_path_parent, "bad_frames", f'{idx[0]:06}.png')
-    #         print(f"Moving {source_path} to {dest_path}")
-    #         os.rename(source_path, dest_path)
-    #         c +=1
-    #     else:
-    #         print(pts1.shape)
+            # os.rename(source_path, dest_path)
+        update_epoch_stats(epoch_stats, img1.detach(), img2.detach(), label.detach(), label.detach(), pts1, pts2, "", data_type="test")
+        
 
     #     # update_epoch_stats(epoch_stats, img1.detach(), img2.detach(), label.detach(), label.detach(), pts1, pts2, "", data_type="test")
     # print(c)
     # print(len(test_loader))
 
 
-    for i, (img1, img2, label, pts1, pts2, seq_name, seq_path, idx) in enumerate(train_loader):
-        if img1 == None:
-            seq_path_parent = os.path.dirname(seq_path[0])
-            source_path = os.path.join(seq_path[0], f'{idx[0]:06}.jpg')
-            dest_path = os.path.join(seq_path_parent, "bad_frames", f'{idx[0]:06}.png')
-            os.makedirs(os.path.join(seq_path_parent, "bad_frames"), exist_ok=True)
 
-            print(f"Moving {source_path} to {dest_path}")
-            os.rename(source_path, dest_path)
-            c +=1
-        else:
-            print(pts1.shape)
-
-        # update_epoch_stats(epoch_stats, img1.detach(), img2.detach(), label.detach(), label.detach(), pts1, pts2, "", data_type="test")
 
     
     print(f'test_algebraic_pred: {epoch_stats["test_algebraic_pred"]/(c+1)}')
