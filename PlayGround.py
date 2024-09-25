17b8ddc18d3e8f0412fd677fe76ee766b427eb7d
alonkaya <alon.kaya.1@gmail.com>
2024-09-25 15:17:04 +0300

    >

--- PlayGround.py
+++ PlayGround.py
@@ -82,7 +82,7 @@ def valid_indices_of_dataset(train_loader, idx):
         print("Dataset not found for the current batch")
 
 def vis_gt():
-    train_loader, val_loader, test_loader = get_data_loaders(data_ratio= 0.1, batch_size=1)
+    train_loader, val_loader, test_loader = get_data_loaders(train_size=10, batch_size=1):
     total_sed = 0
 
     for i, (img1, img2, label, pts1, pts2, seq_name) in enumerate(test_loader):
