1cf00226c0b9b01199540e03ec35066bb91f7e21
alonkaya <alon.kaya.1@gmail.com>
2024-09-25 13:18:34 +0300

    .

--- Dataset.py
+++ Dataset.py
@@ -404,6 +404,7 @@ def save_keypoints_realestate():
 
                 for idx in valid_indices:
                     a = torch.tensor(Image.open(os.path.join(sequence_path, f'{valid_indices[idx]:06}.{IMAGE_TYPE}')).size).to(device)
+                    print(a, original_image_size)
                     if a != original_image_size:
                         print(f"Different sizes: {a}, {original_image_size}")
                     # img0 = torchvision.io.read_image(os.path.join(sequence_path, f'{idx:06}.{IMAGE_TYPE}'))
