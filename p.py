3b586d52141829aedc5918a9b3642ae110474806
alonkaya <alon.kaya.1@gmail.com>
2024-09-30 10:09:30 +0300

    .

--- p.py
+++ p.py
@@ -6,7 +6,8 @@ import shutil
 with open('a.txt', 'r') as file:
     # Iterate over each line in the file
     for line in file:
-        par = os.path.dirname(line.strip()).strip()
+        line = line.strip()
+        par = os.path.dirname(line).strip()
         dest = os.path.join(par, 'SED_0.5__L2_1__huber_1__lr_0.0001__conv__CLIP__use_reconstruction_True_drafts').strip()
         # print(f'from {line} to {dest}\n')
         shutil.move(line, dest)
