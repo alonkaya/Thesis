c39bc0199df521c7e0052356e1eda47785890c0b
alonkaya <alon.kaya.1@gmail.com>
2024-11-20 10:04:24 +0200

    .

--- params.py
+++ params.py
@@ -3,14 +3,14 @@ device, RESNET_MODEL_NAME, CLIP_MODEL_NAME = torch.device(f"cuda" if torch.cuda.
 
 # 1242969 output_0.0375_0.025_frozen_8.log
 # 1413056 output_0.05_frozen_8.log
-# 1483279 output_0.015_orig_25000.log
+# 1483631 output_0.015_orig_25000.log
 
 # nohup env CUDA_VISIBLE_DEVICES=0 TORCH_USE_CUDA_DSA=1 python Main.py > output_.log 2>&1 &
 # gpuQ.py submit -d any -p /home/alonkay/Thesis -e alon_env -c "python Main.py  > output_.log 2>&1"
 
 USE_REALESTATE = False
 STEREO = True
-PRETEXT_TRAIN = False
+PRETEXT_TRAIN = True
 MODEL = CLIP_MODEL_NAME 
 
 ### Dataset ###  
@@ -20,7 +20,7 @@ RESIZE = 256
 AUGMENTATION = True
 RANDOM_CROP = True
 INIT_DATA = True 
-FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0,4,8]
+FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0,4]
 
 ### STEREO ###
 train_seqeunces_stereo = [0,2,3,5] #  10840 images 
