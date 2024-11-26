8a24f8f2642292dee5b2f2c153506f59d8f940ad
alonkaya <alon.kaya.1@gmail.com>
2024-11-26 14:31:40 +0200

    >

--- PlayGround.py
+++ PlayGround.py
@@ -291,7 +291,7 @@ def sed_distance_trained():
         output = model.forward(img1, img2)
 
         update_epoch_stats(epoch_stats, img1.detach(), img2.detach(), label.detach(), output, pts1, pts2, data_type="test")
-
+        print(epoch_stats["test_SED_pred"])
         if i == 10: break
     
 
