dc4b294ef586ae324fb818badde4de08a363ee74
alonkaya <alon.kaya.1@gmail.com>
2024-11-26 17:05:46 +0200

    >

--- utils.py
+++ utils.py
@@ -194,8 +194,8 @@ def reverse_transforms(img_tensor, mean=norm_mean, std=norm_std, is_scaled=True)
         This function is called when computing the epipolar error.
     """
     # The mean and std have to be reshaped to [3, 1, 1] to match the tensor dimensions for broadcasting
-    mean = mean.view(-1, 1, 1)
-    std = std.view(-1, 1, 1)
+    mean = mean.numpy().view(-1, 1, 1)
+    std = std.numpy().view(-1, 1, 1)
     img_tensor = (img_tensor * std + mean) * 255 if is_scaled else img_tensor
     # return (img_tensor.permute(1, 2, 0).numpy()).astype(np.uint8)
     return img_tensor.numpy().transpose(1, 2, 0)
