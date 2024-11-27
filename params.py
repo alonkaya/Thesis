c1751bf1f2cb4f45c8abd9d4c121862e9101954b
alonkaya <alon.kaya.1@gmail.com>
2024-11-27 11:57:49 +0200

    .

--- params.py
+++ params.py
@@ -28,7 +28,7 @@ train_seqeunces_stereo = [0,2,3,5] #  10840 images
 val_sequences_stereo =  [6,7,8]    #  3682 images
 test_sequences_stereo = [9]        #  1064 images
 SEQ_RATIOS = [0.025]     # 3251, 2166, 1082, 540, 405, 269, 161                                                    
-PART = ["mid"] 
+PART = ["tail"] 
 
 ### MONKAA ###
 train_seqeunces_monkaa =  ["treeflight_augmented0_x2", "treeflight_augmented1_x2", "lonetree_winter_x2", "a_rain_of_stones_x2", "eating_naked_camera2_x2",  "family_x2", "lonetree_difftex_x2"]  # 1137
@@ -52,7 +52,7 @@ IMAGE_TYPE = "jpg" if USE_REALESTATE else "png"
 NUM_WORKERS = 0 
 SAVE_MODEL = True
 GET_OLD_PATH = False
-SEED = [42, 300, 500]
+SEED = [300]
 LR = [1e-4]             
 
 ### Epipolar geometry ###
