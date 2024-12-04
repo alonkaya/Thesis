fd7419e8230e96ac1af161715c48c43db3f969f0
alonkaya <alon.kaya.1@gmail.com>
2024-12-04 10:30:46 +0200

    .

--- FMatrixRegressor.py
+++ FMatrixRegressor.py
@@ -460,7 +460,7 @@ SED_truth: {epoch_stats["SED_truth"]}\t\t val_SED_truth: {epoch_stats["val_SED_t
                 epoch_output = f'Epoch {epoch+1}/10: Test SED dist: {epoch_stats["test_SED_pred"]}'
                 print_and_write(epoch_output, self.plots_path) if write else print(epoch_output)
                 
-        output = f"""## TEST RESULTS: ##
+        output = f"""\n\n## TEST RESULTS: ##
 Test Loss: {loss/10}\t\t Test MAE: {mae/10}
 Test Algebraic dist: {alg/10}
 Test SED dist: {sed/10}
