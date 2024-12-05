858a82e61eb754401cedd07cadb5ef61dbe274e5
alonkaya <alon.kaya.1@gmail.com>
2024-12-05 21:34:30 +0200

    >

--- params.py
+++ params.py
@@ -9,12 +9,12 @@ STEREO = True
 PRETEXT_TRAIN = False
 SCENEFLOW = True
 FLYING = True
-MODEL = RESNET_MODEL_NAME 
+MODEL = CLIP_MODEL_NAME 
 FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE or SCENEFLOW else [0,4,8]
 COMPUTER = 1 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 SEQ_RATIOS = [150]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
 LR = [1e-4]             
-KITTI2SCENEFLOW = False
+KITTI2SCENEFLOW = True
 FRESH_MLP = False
 FRESH_CONV = False
 ADDITIONS = "__fresh_MLP_CONV" if FRESH_CONV and FRESH_MLP else "__fresh_MLP" if FRESH_MLP else ""  ## REMEMBER TO PUT "__" !!!!!
