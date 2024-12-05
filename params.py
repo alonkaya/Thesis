540a5479d61e5b5059adf26b60b65a1d2d4a98ab
alonkaya <alon.kaya.1@gmail.com>
2024-12-05 11:26:20 +0200

    .

--- params.py
+++ params.py
@@ -13,7 +13,7 @@ FLYING = False
 MODEL = CLIP_MODEL_NAME 
 FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE or SCENEFLOW else [0,4,8]
 COMPUTER = 1 # 0=132.72.49.250 1=else
-SEQ_RATIOS = [0.2]     # 3251, 2166, 1082, 540, 405, 269, 161                                                    
+SEQ_RATIOS = [0.1]     # 3251, 2166, 1082, 540, 405, 269, 161                                                    
 LR = [1e-4]             
 FRESH_MLP = False
 FRESH_CONV = False
