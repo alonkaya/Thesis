9d720c51df9ce0048f9ed4398ea2c7592f911fee
alonkaya <alon.kaya.1@gmail.com>
2024-12-07 20:19:38 +0200

    >

--- params.py
+++ params.py
@@ -12,7 +12,7 @@ MODEL = RESNET_MODEL_NAME
 FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0,4,8]
 COMPUTER = 1 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 SEQ_RATIOS = [0.025] if not SCENEFLOW else [150] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
-KITTI2SCENEFLOW = True
+KITTI2SCENEFLOW = False
 FRESH_MLP = False
 FIX_BLUR = True
 ONLY_CONTINUE = False
