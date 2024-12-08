de948ad8526ab701c77fd87440cf1ff87ecbac9f
alonkaya <alon.kaya.1@gmail.com>
2024-12-08 09:28:15 +0200

    .

--- params.py
+++ params.py
@@ -7,15 +7,15 @@ STEREO = True
 
 PRETEXT_TRAIN = False
 SCENEFLOW = True
-FLYING = True
+FLYING = False
 MODEL = CLIP_MODEL_NAME 
 FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0,4,8]
-COMPUTER = 1 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-SEQ_RATIOS = [0.025] if not SCENEFLOW else [150] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
-KITTI2SCENEFLOW = True
-FRESH_MLP = True
-FIX_BLUR = True
-ONLY_CONTINUE = False
+COMPUTER = 0 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SEQ_RATIOS = [0.2, 0.1, 0.05, 0.0375, 0.025, 0.015] if not SCENEFLOW else [150] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
+KITTI2SCENEFLOW = False
+FRESH_MLP = False
+FIX_BLUR = False
+ONLY_CONTINUE = True
 ADDITIONS = "__fresh_MLP" if FRESH_MLP else ""  ## REMEMBER TO PUT "__" !!!!!
 ADDITIONS += "__fixed_blur" if FIX_BLUR else ""
 
