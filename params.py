626d24b7a0ade45c691a8581dcb163a6e6dd86a1
alonkaya <alon.kaya.1@gmail.com>
2024-12-08 09:28:46 +0200

    .

--- params.py
+++ params.py
@@ -6,7 +6,7 @@ STEREO = True
 # gpuQ.py submit -d any -p /home/alonkay/Thesis -e alon_env -c "python Main.py  > output_.log 2>&1"
 
 PRETEXT_TRAIN = False
-SCENEFLOW = True
+SCENEFLOW = False
 FLYING = False
 MODEL = CLIP_MODEL_NAME 
 FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0,4,8]
