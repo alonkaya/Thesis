a270522b05d1b197fc8d10fa6d9f9cd9e29a527a
alonkaya <alon.kaya.1@gmail.com>
2024-12-09 09:56:03 +0200

    >

--- params.py
+++ params.py
@@ -6,16 +6,16 @@ STEREO = True
 # gpuQ.py submit -d any -p /home/alonkay/Thesis -e alon_env -c "python Main.py  > output_.log 2>&1"
 
 PRETEXT_TRAIN = False
-SCENEFLOW = False
-FLYING = False
-MODEL = RESNET_MODEL_NAME 
-FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0,4,8]
+SCENEFLOW = True
+FLYING = True
+MODEL = CLIP_MODEL_NAME 
+FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0] if FLYING else [0,4,8]
 COMPUTER = 1 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-SEQ_RATIOS = [0.015] if not SCENEFLOW else [150] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
-KITTI2SCENEFLOW = False
+SEQ_RATIOS = [0.015] if not SCENEFLOW else [9] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
+KITTI2SCENEFLOW = True
 FRESH_MLP = False
 FIX_BLUR = False
-ONLY_CONTINUE = True
+ONLY_CONTINUE = False
 ADDITIONS = "__fresh_MLP" if FRESH_MLP else ""  ## REMEMBER TO PUT "__" !!!!!
 ADDITIONS += "__fixed_blur" if FIX_BLUR else ""
 
