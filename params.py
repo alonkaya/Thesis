f37f48ecaa0146cdee90bd1f1cc6bbdc96cf0d83
alonkaya <alon.kaya.1@gmail.com>
2024-12-13 10:48:17 +0200

    .

--- params.py
+++ params.py
@@ -6,11 +6,11 @@ STEREO = True
 # gpuQ.py submit -d any -p /home/alonkay/Thesis -e alon_env -c "python Main.py  > output_.log 2>&1"
 
 
-PRETEXT_TRAIN = True
+PRETEXT_TRAIN = False
 SCENEFLOW = False
 FLYING = False
-MODEL = CLIP_MODEL_NAME 
-FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [6] if FLYING else [8]
+MODEL = RESNET_MODEL_NAME 
+FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [6] if FLYING else [0]
 COMPUTER = 1 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 SEQ_RATIOS = [0.004, 0.008, 0.1, 0.2] if not SCENEFLOW else [9] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
 KITTI2SCENEFLOW = False
