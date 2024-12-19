81a2a2f631c0fc444c62296e4d98755c11de3aa5
alonkaya <alon.kaya.1@gmail.com>
2024-12-19 17:05:39 +0200

    .

--- params.py
+++ params.py
@@ -11,7 +11,7 @@ PRETEXT_TRAIN = False
 SCENEFLOW = True
 FLYING = True
 MODEL = CLIP_MODEL_NAME 
-FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0] if FLYING else [0,4,8]
+FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [6] if FLYING else [0,4,8]
 FROZEN_HIGH_LAYERS = 0
 COMPUTER = 1 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 MID_SIZES = False
