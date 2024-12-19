b73f9747949602a7d0050e3e07dd2c618930a50e
alonkaya <alon.kaya.1@gmail.com>
2024-12-19 17:06:06 +0200

    .

--- params.py
+++ params.py
@@ -15,7 +15,7 @@ FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [6] if FL
 FROZEN_HIGH_LAYERS = 0
 COMPUTER = 1 # 0=132.72.49.250 1=else  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 MID_SIZES = False
-SEQ_RATIOS = [0.015,0.025,0.0375,0.05] if MID_SIZES and not SCENEFLOW else [0.004,0.008,0.1,0.2] if not SCENEFLOW else [9] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
+SEQ_RATIOS = [0.015,0.025,0.0375,0.05] if MID_SIZES and not SCENEFLOW else [0.004,0.008,0.1,0.2] if not SCENEFLOW else [150] if FLYING else [1]     # 2166, 1082, 540, 405, 269, 161, 88, 47                                                 
 KITTI2SCENEFLOW = False
 ONLY_CONTINUE = False
 PART = ["head", "mid", "tail"] 
