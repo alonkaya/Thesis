e6c489a6e95872d8b3d910d64bd7e3ee3b22551e
alonkaya <alon.kaya.1@gmail.com>
2025-01-14 11:47:35 +0200

    .

--- PlayGround.py
+++ PlayGround.py
@@ -840,10 +840,13 @@ def RANSAC():
     for i, (img1, img2, label, pts1, pts2, _) in enumerate(train_loader):
         pts1 = pts1.cpu().numpy()
         pts2 = pts2.cpu().numpy()
-
+        print(f'pts1 shape: {pts1.shape}, pts2 shape: {pts2.shape}')
         F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_RANSAC, 0.1, 0.99)
+        print(f'F: {F}')
+
         F = torch.from_numpy(F).unsqueeze(0).to(device)
-        print(F.shape)
+        pts1 = torch.from_numpy(pts1).to(device)
+        pts2 = torch.from_numpy(pts2).to(device)
         ep = EpipolarGeometry(img1, img2, F, pts1, pts2)
 
         alg = ep.get_algebraic_distance()
