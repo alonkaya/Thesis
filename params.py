ab6e6f1aefc60a39095e36b7ef55c0aa0dbab669
alonkaya <alon.kaya.1@gmail.com>
2025-01-24 10:36:19 +0200

    .

--- params.py
+++ params.py
@@ -13,7 +13,7 @@ MODEL = CLIP_MODEL_NAME
 FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0] if FLYING else [0]
 FROZEN_HIGH_LAYERS = 0
 COMPUTER = 1 # 0 = 250  1 = 146  2 = else  
-SEQ_RATIOS = [0.002] if not SCENEFLOW else [170, 80] if FLYING else None     # [0.002, 0.004, 0.008, 0.015, 0.025, 0.0375, 0.05, 0.1, 0.2]  /  [9, 80, 170]                                             
+SEQ_RATIOS = [0.002] if not SCENEFLOW else [9, 80] if FLYING else None     # [0.002, 0.004, 0.008, 0.015, 0.025, 0.0375, 0.05, 0.1, 0.2]  /  [9, 80, 170]                                             
 KITTI2SCENEFLOW = False
 ONLY_CONTINUE = False
 PART = ["head", "mid", "tail"] 
