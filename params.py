a5a35dc0a51b169fc8d979ac59246c7c137926cc
alonkaya <alon.kaya.1@gmail.com>
2025-01-25 10:12:31 +0200

    .

--- params.py
+++ params.py
@@ -9,10 +9,10 @@ STEREO = True
 PRETEXT_TRAIN = False
 SCENEFLOW = True
 FLYING = True
-MODEL = CLIP_MODEL_NAME
+MODEL = CLIP_MODEL_NAME_16
 FROZEN_LAYERS = [0] if MODEL==RESNET_MODEL_NAME or USE_REALESTATE else [0] if FLYING else [0]
 FROZEN_HIGH_LAYERS = 0
-COMPUTER = 1 # 0 = 250  1 = 146  2 = else  
+COMPUTER = 2 # 0 = 250  1 = 146  2 = else  
 SEQ_RATIOS = [0.002] if not SCENEFLOW else [9, 80] if FLYING else None     # [0.002, 0.004, 0.008, 0.015, 0.025, 0.0375, 0.05, 0.1, 0.2]  /  [9, 80, 170]                                             
 KITTI2SCENEFLOW = False
 ONLY_CONTINUE = False
