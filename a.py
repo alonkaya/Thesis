f0b445557414395e143c9de06222aab07488db1f
alonkaya <alon.kaya.1@gmail.com>
2025-01-28 18:15:29 +0200

    .

--- a.py
+++ a.py
@@ -4,6 +4,6 @@ for line in open("models.txt"):
     line = line.strip()
     if os.path.exists(line):
         print(f'removing {line}')
-        # os.remove(line)
+        os.remove(line)
     else:
         print(f'does not exist {line}')
