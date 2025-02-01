1285eecb4a4637446c57e7df3b53ef0076f7e6b7
alonkaya <alon.kaya.1@gmail.com>
2025-02-01 13:15:21 +0200

    .

--- b.py
+++ b.py
@@ -132,7 +132,8 @@ if __name__ == '__main__':
     # img1 = transform(img1).unsqueeze(0)  
     # img2 = transform(img2).unsqueeze(0)  
 
-    pretrained_path = "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__lr_0.0001__conv__CLIP__use_reconstruction_True/BS_8__ratio_0.2__head__frozen_0"
+    # pretrained_path = "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__lr_0.0001__conv__CLIP_16__use_reconstruction_True/BS_8__ratio_0.2__head__frozen_0"
+    pretrained_path = "plots/Stereo/Winners/SED_0.5__L2_1__huber_1__lr_0.0001__conv__CLIP__use_reconstruction_True/BS_8__ratio_0.2__mid__frozen_0"
 
     batch_size=1
     _, _, test_loader = get_data_loaders(train_size=0.002, part='head', batch_size=batch_size)
