8ea63baa3896a5b7a771ac9eb68a3fd6d00ac867
alonkaya <alon.kaya.1@gmail.com>
2025-02-03 18:19:15 +0200

    .

--- params.py
+++ params.py
@@ -16,7 +16,7 @@ COMPUTER = 2 # 0 = 250  1 = 146  2 = else
 SEQ_RATIOS = [0.002, 0.004, 0.008, 0.015, 0.025, 0.0375, 0.05, 0.1, 0.2] if not SCENEFLOW else [22] if FLYING else None     # [0.002, 0.004, 0.008, 0.015, 0.025, 0.0375, 0.05, 0.1, 0.2]  /  [9, 80, 170]                                             
 KITTI2SCENEFLOW = False
 ONLY_CONTINUE = False
-PART = ["mid"] 
+PART = ["tail"] 
 MAX_POOL_SIZE = 7 if MODEL==CLIP_MODEL_NAME_16 or MODEL==DINO else 3 ######################################################################
 ADDITIONS = "__Max" if MAX_POOL_SIZE==4 else "" ## REMEMBER TO PUT "__" !!!!!
 ADDITIONS += "__correct_F" if FLYING else ""    #  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
