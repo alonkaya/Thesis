8d5e0965bc634664f5feb9b5e80ea990ccf63262
alonkaya <alon.kaya.1@gmail.com>
2025-02-04 15:44:39 +0200

    .

--- delete.py
+++ delete.py
@@ -2,16 +2,26 @@ import os
 import shutil
 
 # Specify the path to the main folder containing all subfolders
-main_folder_path = "/mnt/sda2/Alon/plots/Stereo/Winners/"
+main_folder_path = "plots/Flying"
 for f in os.listdir(main_folder_path):
     folder_name = os.path.join(main_folder_path, f)
     # Iterate over all folders inside the main folder
     for subfolder_name in os.listdir(folder_name):
         subfolder_path = os.path.join(folder_name, subfolder_name)
         # Ensure we are only processing directories
-        if os.path.isdir(subfolder_path) and (subfolder_name.endswith("bad") or subfolder_name.endswith("Max")):
-            print(subfolder_path)
-            shutil.rmtree(subfolder_path)
+        if os.path.isdir(subfolder_path) and (subfolder_name.endswith("bad") or "correct_F" not in subfolder_name):
+            comp0 = os.path.join("/mnt/sda2/Alon", subfolder_path)
+            comp1 = os.path.join("/mnt_hdd15tb/alonkay/Thesis/", subfolder_path)
+            if os.exists(comp0):
+                # shutil.rmtree(comp0)
+                print(f"Deleted: {comp0}")
+            elif os.exists(comp1):
+                # shutil.rmtree(comp1)
+                print(f"Deleted: {comp1}")
+            else:
+                # shutil.rmtree(subfolder_path)
+                print(f"Deleted: {subfolder_path}")
+
 
             # model_path = os.path.join(subfolder_path, 'model.pth')
             # backup_path = os.path.join(subfolder_path, 'backup_model.pth')
