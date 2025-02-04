900f3019a4c397617d117b4e90f37d0681616478
alonkaya <alon.kaya.1@gmail.com>
2025-02-04 16:02:19 +0200

    .

--- a.py
+++ a.py
@@ -1,7 +1,10 @@
 from params import *
 import os
+comp1 = "/mnt_hdd15tb/alonkay/Thesis/"
+comp0 = "/mnt/sda2/Alon/"
+
 for p in [FLYING_MODEL_CLIP_PATH, FLYING_MODEL_CLIP_16_PATH, FLYING_MODEL_DINO_PATH, FLYING_MODEL_RESNET_PATH, FLYING_MODEL_EFFIEICNT_PATH]:
-    comp1_p = os.path.join("", p)
+    comp1_p = os.path.join(comp0, p)
     if os.path.exists(comp1_p):
         print(f"Found in comp1: {comp1_p}")
     else:
