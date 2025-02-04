b093aab0447eb22ace241e6c70e6c1305a447595
alonkaya <alon.kaya.1@gmail.com>
2025-02-04 15:59:23 +0200

    .

---
+++ a.py
@@ -0,0 +1,8 @@
+from params import *
+import os
+for p in [FLYING_MODEL_CLIP_PATH, FLYING_MODEL_CLIP_16_PATH, FLYING_MODEL_DINO_PATH, FLYING_MODEL_RESNET_PATH, FLYING_MODEL_EFFIEICNT_PATH]:
+    comp1_p = os.path.join("/mnt_hdd15tb/alonkay/Thesis/", p)
+    if os.path.exists(comp1_p):
+        print(f"Found in comp1: {comp1_p}")
+    else:
+        print(f"Not found in comp1: {comp1_p}")
